82542bbdfb8ec85e75153253e4ed1ffb240c088c
Alemnew Marie <alemnewmarie461@gmail.com>
2023-12-13 16:13:16 +0300

    Added user model

--- skimmit/settings.py
+++ skimmit/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     # 3rd party
     # local
     "url_summary",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
