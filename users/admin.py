f996419c1d3df78c5841eacde694149b591b442b
Alemnew Marie <alemnewmarie461@gmail.com>
2023-12-23 15:23:51 +0300

    added guest login credential

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ("username", "email", "first_name", "last_name")
-    search_fields = ("username", "email", "first_name", "last_name")
-    readonly_fields = ("password", "last_login", "date_joined")
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+
+admin.site.register(User, CustomUserAdmin)
