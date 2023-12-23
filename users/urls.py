0ad092afe3cf531d6950524d4a948e9fac7dcbd3
Alemnew Marie <alemnewmarie461@gmail.com>
2023-12-23 11:43:27 +0300

    added profile and summary detail view

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,5 @@ app_name = "users"
 urlpatterns = [
     path("profile/", views.profile, name="profile"),
     path("preferences/", views.preferences, name="preferences"),
+    path("summary/<int:id>/", views.summary_detail, name="summary_detail"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 
 
+@login_required
 def profile(request):
-    return render(request, "users/profile.html")
+    user = get_user_model().objects.get(id=request.user.id)
+    bookmarks = user.bookmarked_summaries.all()
+    context = {"bookmarks": bookmarks}
+    return render(request, "users/profile.html", context)
 
 
+@login_required
 def preferences(request):
     return render(request, "users/preferences.html")
+
+
+@login_required
+def summary_detail(request, id):
+    user = get_user_model().objects.get(id=request.user.id)
+    bookmark = get_object_or_404(user.bookmarked_summaries, id=id)
+    sentences = [s.strip() for s in bookmark.summary.split("- ") if s.strip()]
+    context = {"bookmark": bookmark, "sentences": sentences}
+    return render(request, "users/summary_detail.html", context)
